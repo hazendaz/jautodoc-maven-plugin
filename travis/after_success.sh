6339b4404f2cdbea22ffdf575716ff4dd64855c0
Jeremy Landis <jeremylandis@hotmail.com>
2018-04-23 23:11:25 -0400

    [ci] Update copyright dates

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#    Copyright ${license.git.copyrightYears} the original author or authors.
+#    Copyright 2011-2018 the original author or authors.
 #
 #     All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
 #     Public License v1.0 which accompanies this distribution, and is available at
