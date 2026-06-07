e8efa434e866f601c87275496555872503fcb446
Jeremy Landis <jeremylandis@hotmail.com>
2026-06-07 19:59:39 -0400

    [license] Move to EPL 2.0 rather than 1.0 as project is new and better to align right away
    inception year also moved to first date I tried this in 2018.

--- src/main/java/com/hazendaz/maven/jautodoc/package-info.java
+++ src/main/java/com/hazendaz/maven/jautodoc/package-info.java
@@ -1,10 +1,8 @@
 /*
- *     Copyright 2011-2026 the original author or authors.
+ * SPDX-License-Identifier: EPL-2.0
+ * See LICENSE file for details.
  *
- *     All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
- *     Public License v1.0 which accompanies this distribution, and is available at
- *
- *     https://www.eclipse.org/legal/epl-v10.html.
+ * Copyright 2018-2026 hazendaz
  */
 /**
  * Contains jautodoc command for this plugin.
